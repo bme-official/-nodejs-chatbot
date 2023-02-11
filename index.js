1f8e2e72122bb6c08c0c5874d8c4dd2c7d8d9803
唐川拓磨 <takuma@Takumas-MacBook-Air.local>
2023-02-11 19:47:47 +0900

    modify

--- index.js
+++ index.js
@@ -75,7 +75,5 @@ client.on(Events.MessageCreate, async (message) => {
     await message.channel.send(response.data.choices[0].text)
 })
 
-process.env.OPENAI;
 process.env.OPENAI_KEY;
-process.env.BOT;
 process.env.BOT_TOKEN;
